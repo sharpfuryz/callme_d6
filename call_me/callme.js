c68005a28c42471bdc64fe8c32af1d1bf9f9fcc9
Sharpfuryz <sharpfuryz@gmail.com>
2013-01-24 20:25:59 +0200

    Ported call_me in modal window, fully workingw

---
+++ call_me/callme.js
@@ -0,0 +1,51 @@
+$(function(){
+jQuery(".callme_viewform").click(function(){
+	callmeShowForm();
+});
+
+jQuery("#cme_cls").click(function(){//кнопка "закрыть" 
+	callmeShowForm();
+});
+
+jQuery("#viewform").hover(function(){ 
+	jQuery(this).addClass("callmeform_hover");
+},function(){
+	jQuery(this).removeClass("callmeform_hover");
+}
+
+);
+});
+
+function callmeShowForm(){ //скрываем и показываем форму
+	jQuery('#callmeform').toggle();
+	jQuery("#callme_result").html("");
+}
+
+function sendMail() {
+var name = $('#cname').val();
+var phone = $('#cphone').val();
+var ccmnt = $('#ccmnt').val();
+$.post('?q=callme/js', {name: name, phone: phone, ccmnt: ccmnt},function(data){
+	var text = "<div class='c_success'>Okay</div>";
+	$('#callme_result').html(text);
+	$('#callmeform .btn').attr("disabled","disabled");
+	$('#callmeform .txt').val("");		
+});
+}
+
+jQuery(document).ready(function(){
+
+jQuery("#callmeform .btn").click(function(){
+	jQuery("#callme_result").html("<div class='sending'>Отправка...</div>");
+	setTimeout( function(){ sendMail(); }, 2000);
+	return false;
+});	
+});
+
+jQuery(document).keyup(function(a) { //обработка esc
+	if (a.keyCode == 27) {
+		if(jQuery("#callmeform").is(":visible")) {
+			callmeShowForm();
+		}
+	}
+});
